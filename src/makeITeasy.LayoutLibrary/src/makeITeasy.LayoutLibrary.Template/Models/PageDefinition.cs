546fb01b91f5fe37e84f238f1a006a35f4998545
Manu D <edesigau@hotmail.fr>
2020-05-01 15:58:00 +0200

    initial commit of sample project

---
+++ src/makeITeasy.LayoutLibrary/src/makeITeasy.LayoutLibrary.Template/Models/PageDefinition.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace makeITeasy.LayoutLibrary.Template.Models
+{
+    public class PageDefinition
+    {
+        public string Title { get; set; }
+    }
+}
---
+++ src/makeITeasy.LayoutLibrary/src/makeITeasy.LayoutLibrary.WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using makeITeasy.LayoutLibrary.Template.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace makeITeasy.LayoutLibrary.WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly PageDefinition pageDefinition;
+
+        public HomeController(PageDefinition pageDefinition)
+        {
+            this.pageDefinition = pageDefinition;
+        }
+
+        public IActionResult Index()
+        {
+            pageDefinition.Title = "My Index Page";
+            return View();
+        }
+    }
+}
