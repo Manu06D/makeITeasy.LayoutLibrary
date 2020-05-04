a6b0ca947a92c5d4c2f35c5a25fcd6e855edbe73
Manu D <edesigau@hotmail.fr>
2020-05-04 13:34:38 +0200

    Add live debug support for Template project

--- src/makeITeasy.LayoutLibrary/src/makeITeasy.LayoutLibrary.WebApplication/Startup.cs
+++ src/makeITeasy.LayoutLibrary/src/makeITeasy.LayoutLibrary.WebApplication/Startup.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using makeITeasy.LayoutLibrary.Template.Models;
 using Microsoft.AspNetCore.Builder;
@@ -8,15 +11,19 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace makeITeasy.LayoutLibrary.WebApplication
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IWebHostEnvironment _env;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
 
         public IConfiguration Configuration { get; }
@@ -24,8 +31,23 @@ namespace makeITeasy.LayoutLibrary.WebApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
             services.AddScoped<PageDefinition>();
+            var builder = services.AddControllersWithViews();
+
+#if DEBUG
+            if (_env.IsDevelopment())
+            {
+                string[] dirs = { Assembly.GetExecutingAssembly().GetName().Name, typeof(PageDefinition).Assembly.GetName().Name };
+                builder.AddRazorRuntimeCompilation(options =>
+                {
+                    foreach (var dir in dirs)
+                    {
+                        var libraryPath = Path.GetFullPath(Path.Combine(_env.ContentRootPath, "..", dir));
+                        options.FileProviders.Add(new PhysicalFileProvider(libraryPath));
+                    }
+                });
+            }
+#endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
